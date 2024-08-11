c9954b4a682a5a86a01396387515f4f32b9544fa
Fatemeh Akhlaghi <akhlaghi.fatemeh@gmail.com>
2024-08-11 11:24:42 +0330

    change nextjs config and next.yaml

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    distDir: 'build',
+    generateBuildId: async () => {
+        return 'ver-1-' + new Date().getTime()
+    }
+};
 
 export default nextConfig;

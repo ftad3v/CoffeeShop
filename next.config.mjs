fb481dc01b7ca77adc70d99b9316136bc5edef36
Fatemeh Akhlaghi <akhlaghi.fatemeh@gmail.com>
2024-08-11 14:40:10 +0330

    change config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    distDir: 'build',
-    generateBuildId: async () => {
-        return 'ver-1-' + new Date().getTime()
-    }
+    // distDir: 'build',
+    // generateBuildId: async () => {
+    //     return 'ver-1-' + new Date().getTime()
+    // }
 };
 
 export default nextConfig;
